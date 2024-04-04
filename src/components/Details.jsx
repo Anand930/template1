63828a4f08ba3340c8be2cca85ffdce9e653529f
Anand930 <88427835+Anand930@users.noreply.github.com>
2024-04-04 11:45:50 +0000

    codespace 1st

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Details.css";
 import mail from "../assets/email-icon.png";
+import work from '../assets/work-icon.png'
+import facebook from "../assets/facebook.svg"
+import planeteria from "../assets/planeteria.svg"
+import starbucks from "../assets/starbucks.svg"
+import airbnb from "../assets/airbnb.svg"
 
 const Details = () => {
   return (
@@ -63,6 +68,73 @@ const Details = () => {
           <div className="first-container-second-section content">
             <input className="first-container-input" placeholder="Email Address" type="text" />
             <button>Join</button>
+          </div>
+        </div>
+        <div className="second-container">
+          <div className="second-container-img">
+            <img src={work} length={"25px"} width={"25px"} alt="" />
+            <p>Work</p>
+          </div>
+          <div className="experience-container">
+            <div className="planeria-container img-child-container">
+              <img src={planeteria} alt="planeteria" className="planeteria" />
+              <div className="img-right-container">
+              <div className="heading">
+                <p>Planeria</p>
+              </div>
+              
+              <div className="designation">
+                CEO
+                <div className="duration">
+                  2019 - Present
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className="airbnb-container img-child-container">
+              <img src={airbnb} alt="airbnb" className="airbnb" />
+              <div className="img-right-container">
+              <div className="heading">
+                <p>Airbnb</p>
+              </div>
+              <div className="designation">
+                CEO
+                <div className="duration">
+                  2019 - Present
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className="facebook-container img-child-container">
+              <img src={facebook} alt="facebook" className="facebook" />
+              <div className="img-right-container">
+              <div className="heading">
+                <p>facebook</p>
+              </div>
+              <div className="designation">
+                
+              <div className="duration">
+                  2019 - Present
+                </div>
+              </div>
+              </div>
+
+            </div>
+            <div className="starbucks-container img-child-container heading">
+              <img src={starbucks} alt="starbucks" className="starbucks" />
+              <div className="img-right-container">
+                <div className="heading">
+                  <p>StarBucks</p>
+                </div>
+                <div className="designation">
+                  CEO
+                  <div className="duration">
+                  2019 - Present
+                </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
